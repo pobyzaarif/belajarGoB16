56bd2f873914e8bb34204db58a66911ff40ce397
Poby Zaarifwandono <pobyzarif34@gmail.com>
2026-05-26 21:09:53 +0700

    add ddl database

---
+++ sql/database.sql
@@ -0,0 +1,12 @@
+-- Preliminary
+CREATE DATABASE campus;
+
+use campus;
+
+-- DDL
+CREATE TABLE IF NOT EXISTS students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), age INT);
+
+-- DML 
+-- seeder
+
+-- query business logic
